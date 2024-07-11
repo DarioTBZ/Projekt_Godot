ee9559e84bdcc0b4687a9270ff6f2d4a4c7e5409
Dario Kulici <dariokulici@MacBook-von-Dario.local>
2024-07-11 10:08:50 +0200

    Update

--- Action_Arena/scripts/main.gd
+++ Action_Arena/scripts/main.gd
@@ -3,6 +3,6 @@ extends Node2D
 @onready var Locked = $NavigationRegion2D/TileMap/Area2D/Locked
 var paused = false
 
-func _process(delta):
+func _process(_delta):
 	if Global.score >= 10:
 		Locked.text = "UNLOCKED"
