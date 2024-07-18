8b9a044b41cf9dacd0df52f30660144a549217dd
2iqmartin <sorrentimartin@gmail.com>
2024-07-18 15:29:58 +0200

    more "realistic" gun

--- Action_Arena/scripts/pause_game.gd
+++ Action_Arena/scripts/pause_game.gd
@@ -1,17 +1,10 @@
 extends Node
 
-var paused = false
-@onready var pause_screen = %PauseScreen
-
 func _process(_delta):
 		# Pause the Game
 	if Input.is_action_just_pressed("pause"):
-		if paused == true:
-			paused = false
-			get_tree().paused = false
-			pause_screen.visible = false
-		else:
+		if get_tree().paused == false:
 			get_tree().paused = true
-			paused = true
-			pause_screen.visible = true
+		else: 
+			get_tree().paused = false
 
--- Action_Arena/scripts/pause_screen.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasLayer
-
-
-
-func quitbutton_pressed():
-	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
