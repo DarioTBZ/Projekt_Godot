22c4c6b47c60835b566ee7ee1955995e27a6ac16
Dario Kulici <dariokulici@MacBook-von-Dario.local>
2024-12-18 18:53:59 +0100

    Removed print on global_music_player

--- jump'n-run/scripts/music/music_player.gd
+++ jump'n-run/scripts/music/music_player.gd
@@ -2,8 +2,6 @@ extends Node
 
 @onready var music_player = get_node("AudioStreamPlayer")
 
-func _ready():
-	print("Music Player Node: ", music_player)
 
 func play_music(stream: AudioStream):
 	music_player.stream = stream
