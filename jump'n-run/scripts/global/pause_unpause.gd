b94a598a759900be04518b4c179060ea6707f974
DarioPrivat <dario.kulici@edu.tbz.ch>
2024-12-19 22:33:36 +0100

    Worked on Death Screen

--- jump'n-run/scripts/global/pause_unpause.gd
+++ jump'n-run/scripts/global/pause_unpause.gd
@@ -2,7 +2,7 @@ extends Node
 
 var is_paused:bool = false
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("PauseUnpause"):
 		is_paused = !is_paused
 		get_tree().paused = is_paused
