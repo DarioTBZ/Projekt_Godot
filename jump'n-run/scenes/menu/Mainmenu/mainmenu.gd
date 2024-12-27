bd4bcde36314489ecc23ba107d89350ba5be1cb5
DarioPrivat <dario.kulici@edu.tbz.ch>
2024-12-27 17:11:18 +0100

    Added Settings Screen

--- jump'n-run/scenes/menu/Mainmenu/mainmenu.gd
+++ jump'n-run/scenes/menu/Mainmenu/mainmenu.gd
@@ -17,3 +17,7 @@ func gamestart_button_pressed() -> void:
 
 func quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu/SettingsMenu/settings.tscn")
