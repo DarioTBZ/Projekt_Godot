360d99f6667d0c6fb73311bf7a43843edd21a49c
DarioPrivat <dario.kulici@edu.tbz.ch>
2025-01-01 02:19:29 +0100

    Added invisible wall

--- jump'n-run/scenes/animation/level-animation.gd
+++ jump'n-run/scenes/animation/level-animation.gd
@@ -1,11 +1,18 @@
 extends AnimationPlayer
 
 signal ui_switch
+signal disable_music_switch
 
 @onready var area_2d: Area2D = $Area2D
 @onready var camera: Camera2D = $Camera2D
 
+@onready var music_change: Node = null
+
 func _ready() -> void:
+	music_change = get_node("/root/Level1/MusicChange")
+	
+	connect("disable_music_switch", Callable(music_change, "on_animation_finished"))
+	
 	camera.enabled = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -19,3 +26,4 @@ func _on_animation_finished(_anim_name: StringName) -> void:
 	area_2d.monitoring = false
 	GlobalMusicPlayer.scnd_set_volume(-10)
 	emit_signal("ui_switch")
+	emit_signal("disable_music_switch")
--- jump'n-run/scenes/sound/level1_music_change/music_change.gd
+++ jump'n-run/scenes/sound/level1_music_change/music_change.gd
@@ -7,30 +7,31 @@ var creepy_theme = load("res://assets/music/horror_background.wav")
 @onready var area_2d_4: Area2D = $Area2D4
 @onready var sound_lauter: Area2D = $SoundLauter
 
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		GlobalMusicPlayer.set_volume(-8)
-		area_2d.call_deferred("set", "monitoring", false)
-
 
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		GlobalMusicPlayer.set_volume(-12)
-		area_2d_2.call_deferred("set", "monitoring", false)
 
 func _on_body_entered_phase2(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		GlobalMusicPlayer.set_volume(-22)
-		area_2d_3.call_deferred("set", "monitoring", false)
 
 func _on_body_entered_phase3(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		GlobalMusicPlayer.main_pause_player(true)
 		GlobalMusicPlayer.scnd_set_volume(9)
 		GlobalMusicPlayer.scnd_play_music(creepy_theme)
-		area_2d_4.call_deferred("set", "monitoring", false)
-
 
 func _on_sound_lauter_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		GlobalMusicPlayer.set_volume(-5)
+
+func on_animation_finished():
+	area_2d.call_deferred("set", "monitoring", false)
+	area_2d_2.call_deferred("set", "monitoring", false)
+	area_2d_3.call_deferred("set", "monitoring", false)
+	area_2d_4.call_deferred("set", "monitoring", false)
